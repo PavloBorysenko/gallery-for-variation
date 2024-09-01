5ab6c371c5873dfe641e1f67f40b8a7d6fffd1a1
Pavlo Borysenko <irod-09@mail.ru>
2024-09-01 14:57:32 +0200

    end test-GalleryVideo

--- tests/phpunit/tests/test-GalleryVideo.php
+++ tests/phpunit/tests/test-GalleryVideo.php
@@ -1,14 +1,13 @@
 <?php
 
-class Test_GalleryImage extends WP_UnitTestCase {
+class Test_GalleryVideo extends WP_UnitTestCase {
 
 	public static $attachment_ids;
 	public function setUp(): void {
 		parent::setUp();
 	}
 	public static function wpSetUpBeforeClass( $factory ) {
-		self::$attachment_ids[] = $factory->attachment->create_upload_object( GFV_TEST_PATH . '/files/wp-logo.png', 0 );
-		self::$attachment_ids[] = $factory->attachment->create_upload_object( GFV_TEST_PATH . '/files/wp-logo.jpg', 0 );
+		self::$attachment_ids[] = $factory->attachment->create_upload_object( GFV_TEST_PATH . '/files/clip.mp4', 0 );
 	}
 	public static function wpTearDownAfterClass() {
 		foreach ( self::$attachment_ids as $id ) {
@@ -24,24 +23,24 @@ class Test_GalleryImage extends WP_UnitTestCase {
 	 * @dataProvider index_data_provider
 	 */
 	public function test_get_origin_src( $index ) {
-		$expected_src = wp_get_attachment_image_url( self::$attachment_ids[ $index ] );
+		$expected_src = wp_get_attachment_url( self::$attachment_ids[ $index ] );
 		$post = $this->factory->post->get_object_by_id( self::$attachment_ids[ $index ] );
-		$gallery_image = new \ParadigmaTools\Gfv\Items\Item\GalleryImage( $post );
+		$gallery_video = new \ParadigmaTools\Gfv\Items\Item\GalleryVideo( $post );
 
-		$this->assertSame( $expected_src, $gallery_image->get_origin_src(),
-			"Expected URL of original image." );
+		$this->assertSame( $expected_src, $gallery_video->get_origin_src(),
+			"Expected URL of original video." );
 
 	}
 	/**
 	 * @dataProvider index_data_provider
 	 */
 	public function test_get_icon_src( $index ) {
-		$expected_src = wp_get_attachment_image_url( self::$attachment_ids[ $index ], 'thumbnail' );
 		$post = $this->factory->post->get_object_by_id( self::$attachment_ids[ $index ] );
-		$gallery_image = new \ParadigmaTools\Gfv\Items\Item\GalleryImage( $post );
+		$expected_src = wp_mime_type_icon( self::$attachment_ids[ $index ] );
+		$gallery_video = new \ParadigmaTools\Gfv\Items\Item\GalleryVideo( $post );
 
-		$this->assertSame( $expected_src, $gallery_image->get_icon_src(),
-			"Expected image thumbnail URL." );
+		$this->assertSame( $expected_src, $gallery_video->get_icon_src(),
+			"Expected video icon." );
 	}
 	/**
 	 * @dataProvider index_data_provider
@@ -49,19 +48,19 @@ class Test_GalleryImage extends WP_UnitTestCase {
 	public function test_get_title( $index ) {
 		$expected_title = get_the_title( self::$attachment_ids[ $index ] );
 		$post = $this->factory->post->get_object_by_id( self::$attachment_ids[ $index ] );
-		$gallery_image = new \ParadigmaTools\Gfv\Items\Item\GalleryImage( $post );
+		$gallery_video = new \ParadigmaTools\Gfv\Items\Item\GalleryVideo( $post );
 
-		$this->assertSame( $expected_title, $gallery_image->get_title(),
-			"Incorrect image title." );
+		$this->assertSame( $expected_title, $gallery_video->get_title(),
+			"Incorrect Video title." );
 	}
 	/**
 	 * @dataProvider index_data_provider
 	 */
 	public function test_get_id( $index ) {
 		$post = $this->factory->post->get_object_by_id( self::$attachment_ids[ $index ] );
-		$gallery_image = new \ParadigmaTools\Gfv\Items\Item\GalleryImage( $post );
+		$gallery_video = new \ParadigmaTools\Gfv\Items\Item\GalleryVideo( $post );
 
-		$this->assertSame( self::$attachment_ids[ $index ], $gallery_image->get_id(),
+		$this->assertSame( self::$attachment_ids[ $index ], $gallery_video->get_id(),
 			"Incorrect attachment ID." );
 	}
 	/**
@@ -69,15 +68,14 @@ class Test_GalleryImage extends WP_UnitTestCase {
 	 */
 	public function test_get_type( $index ) {
 		$post = $this->factory->post->get_object_by_id( self::$attachment_ids[ $index ] );
-		$gallery_image = new \ParadigmaTools\Gfv\Items\Item\GalleryImage( $post );
+		$gallery_video = new \ParadigmaTools\Gfv\Items\Item\GalleryVideo( $post );
 
-		$this->assertSame( 'image', $gallery_image->get_type(),
-			"Gallery item type must be image" );
+		$this->assertSame( 'video', $gallery_video->get_type(),
+			"Gallery item type must be video" );
 	}
 	public function index_data_provider(): array {
 		return array(
-			'png' => array( 0 ),
-			'jpg' => array( 1 ),
+			'mp4' => array( 0 ),
 		);
 	}
 }
